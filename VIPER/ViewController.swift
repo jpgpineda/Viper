8a96d215b0c2704d73de4fa0efed402b83958071
Javier Pineda <jpgpineda98@gmail.com>
2022-08-19 08:40:56 -0500

    Initial Commit

---
+++ VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER
+//
+//  Created by Javier Pineda Gonzalez on 19/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
